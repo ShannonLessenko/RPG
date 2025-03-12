f7ca127e883f4caed59e200e9631c9e9aa3b0d1c
Shannon <shannonbaptista29@gmail.com>
2025-03-12 15:17:40 -0400

    RPG

---
+++ main/meuprojeto/src/App.js
@@ -0,0 +1,11 @@
+import CharacterSelection from "./components/CharacterSelection";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <CharacterSelection />
+    </div>
+  );
+}
+
+export default App;
